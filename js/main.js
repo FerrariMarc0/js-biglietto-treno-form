b75f28cf3a1a0a1d4a1c075360c0482d6a5fe6d4
FerrariMarc0 <marco1010@live.it>
2023-02-23 12:10:51 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,48 @@
-'use strict'
-let button = document.getElementById('gen');
-let raceKm = document.getElementById('road').value;
-const priceForKm = 0.21;
-const totPrice = priceForKm * raceKm;
-const juniorDiscount = (totPrice / 100) * 20;
-const seniorDiscount = (totPrice / 100) * 40;
+'use strict';
+
+const button = document.getElementById('gen');
+const resetButton = document.getElementById('hide');
+const userName = document.getElementById('input_name');
+const raceKm = document.getElementById('road');
+const age = document.getElementById('eta');
+const juniorDiscount = 0.8;
+const seniorDiscount = 0.6;
 const overView = document.getElementById('recap');
-const userName = document.getElementById("input_name").value;
 const num = Math.round(Math.random() * 100000);
 const num1 = Math.round(Math.random() * 10);
 
 button.addEventListener('click',
 function(){
-    let inputElement = document.getElementById('input_name').value;
-    document.getElementById("identity").innerHTML = inputElement; 
+    const inputNameValue = userName.value;
+    const inputKmValue = Number(raceKm.value);
+    const inputAgeValue = eta.value;
+
+    console.log(inputNameValue, inputKmValue, inputAgeValue);
+
+    let ticketPrice = 0.21 * inputKmValue;
+
+    console.log({ticketPrice});
+
+    if(inputAgeValue === 'minor'){
+        ticketPrice = ticketPrice * juniorDiscount;
+        console.log(`Ti spetta uno sconto del 20%, il prezzo totale è ${ticketPrice} euro`);
+    } else if(inputAgeValue === 'senior'){
+        ticketPrice = ticketPrice * seniorDiscount;
+        console.log(`Ti spetta uno sconto del 40%, il prezzo totale è ${ticketPrice} euro`);
+    }
+    console.log(`Il prezzo totale è ${ticketPrice} euro`);
+    console.log({ticketPrice});
+    document.getElementById("identity").innerHTML = inputNameValue;
+    document.getElementById("age").innerHTML = inputAgeValue;
+    document.getElementById("price").innerHTML = `${ticketPrice.toFixed(2)}€`;
     document.getElementById("ccp").innerHTML = num;
     document.getElementById("car").innerHTML = num1;
-    overView.classList.toggle('show');
+    
 });
 
+resetButton.addEventListener('click', function(){
+    overView.classList.toggle('show');
+})
 
 
 
